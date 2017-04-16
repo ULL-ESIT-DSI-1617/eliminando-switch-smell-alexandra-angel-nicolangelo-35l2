db9a8e6eb9ab26f78eb8322edb9ce7bace13fdbd
wiwihyuga <angel_luis_tf@hotmail.com>
2017-04-16 17:49:46 +0000

    Haciendo cambios

--- assets/js/temperature.js
+++ assets/js/temperature.js
@@ -7,24 +7,24 @@
 
 // var Medida = require("./medida");
 
-class Medida {
-    constructor(value, string) {
-        this.valor = value;
-        this.cadena = string;
-    }
-    get getValue() {
-        return this.valor;
-    }
-    get getString() {
-        return this.cadena;
-    }
-    set setValue(newValue) {
-        this.valor = newValue;
-    }
-    set setString(newString) {
-        this.cadena = newString;
-    }
-}
+// class Medida {
+//     constructor(value, string) {
+//         this.valor = value;
+//         this.cadena = string;
+//     }
+//     get getValue() {
+//         return this.valor;
+//     }
+//     get getString() {
+//         return this.cadena;
+//     }
+//     set setValue(newValue) {
+//         this.valor = newValue;
+//     }
+//     set setString(newString) {
+//         this.cadena = newString;
+//     }
+// }
 class Temperatura extends Medida {
     constructor(value, string) {
         super(value, string);
@@ -120,28 +120,28 @@ class Kelvin extends Temperatura {
 }
 
 
-function calculate() {
-  var result;
-  var temp = original.value;
-  var regexp = /^([-+])?(\d+)(.\d*)?(e[-+]?\d+)?([cCfFkK])(\sto)?\s([cCfFkK])$/;
-  var m = temp.match(regexp);
-  var num = m[2]; // valor numerico
-  var type1 = m[5]; // primera letra
-  var type2 = m[7]; // segunda letra
-   if (m) {
-        if (type1 == 'c' || type1 == 'C') {
-            var celsius = new Celsius(num, type2);
-            result = celsius.convert();
-        }else if (type1 == 'f' || type1 == 'F') {
-            var farenheit = new Farenheit(num, type2);
-            result = farenheit.convert();
-        }else if (type1 == 'k' || type1 == 'K') {
+// function calculate() {
+//   var result;
+//   var temp = original.value;
+//   var regexp = /^([-+])?(\d+)(.\d*)?(e[-+]?\d+)?([cCfFkK])(\sto)?\s([cCfFkK])$/;
+//   var m = temp.match(regexp);
+//   var num = m[2]; // valor numerico
+//   var type1 = m[5]; // primera letra
+//   var type2 = m[7]; // segunda letra
+//   if (m) {
+//         if (type1 == 'c' || type1 == 'C') {
+//             var celsius = new Celsius(num, type2);
+//             result = celsius.convert();
+//         }else if (type1 == 'f' || type1 == 'F') {
+//             var farenheit = new Farenheit(num, type2);
+//             result = farenheit.convert();
+//         }else if (type1 == 'k' || type1 == 'K') {
              
-            var kelvin = new Kelvin(num, type2);
+//             var kelvin = new Kelvin(num, type2);
             
-            result = kelvin.convert();
-        }
-        console.log(result);
-        converted.innerHTML = result;
-   }
-}
+//             result = kelvin.convert();
+//         }
+//         console.log(result);
+//         converted.innerHTML = result;
+//   }
+// }
